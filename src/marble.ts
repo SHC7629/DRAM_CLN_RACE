87d557ca8218ccc5c55875e381c4a98af7afdd27
SHC7629 <198679659+SHC7629@users.noreply.github.com>
2026-04-13 00:32:48 +0900

    Adjust marble size

--- src/marble.ts
+++ src/marble.ts
@@ -159,7 +159,7 @@ export class Marble {
       transformGuard(ctx, () => {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
-        ctx.drawImage(skin, -hs, -hs, hs * 4, hs * 4);
+        ctx.drawImage(skin, -hs, -hs, hs * 2, hs * 2);
       });
     } else {
       this._drawMarbleBody(ctx, false);
