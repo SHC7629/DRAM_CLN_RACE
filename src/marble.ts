d85557076bf38c57047bfa96a9e2155dc4f442ca
SHC7629 <198679659+SHC7629@users.noreply.github.com>
2026-04-13 00:34:30 +0900

    Adjust marble size

--- src/marble.ts
+++ src/marble.ts
@@ -10,7 +10,7 @@ import { Vector } from './utils/Vector';
 export class Marble {
   type = 'marble' as const;
   name: string = '';
-  size: number = 1;
+  size: number = 0.7;
   color: string = 'red';
   hue: number = 0;
   impact: number = 0;
